22f6ad210a46af195ae00882d28e911042561142
emilmv <emilnm@code.edu.az>
2024-08-05 14:17:05 +0400

    Added Validators for ProductCreateDTO class

--- ShopAppPB301/DTOs/ProductDTOs/ProductCreateDTO.cs
+++ ShopAppPB301/DTOs/ProductDTOs/ProductCreateDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace ShopAppPB301.DTOs.ProductDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopAppPB301.DTOs.ProductDTOs
 {
     public class ProductCreateDTO
     {
+        [MaxLength(10)]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Range(0, 500)]
         public decimal Price { get; set; }
     }
 }
